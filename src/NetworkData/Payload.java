4a9161259bed0bd13c1c5e31a51a7b30e9e9570f
frawg <joelpangsg@outlook.com>
2014-07-30 15:55:48 +0800

    Added Network Data types

---
+++ src/NetworkData/Payload.java
@@ -0,0 +1,17 @@
+package NetworkData;
+
+public class Payload {
+	private int sourceAppPort, destAppPort = 0;
+	private String data = null;
+	
+	public Payload(int source, int dest, String data)
+	{
+		this.data = data;
+		sourceAppPort = source;
+		destAppPort = dest;
+	}
+	
+	public String getData(){ return data; }
+	public int getSourcePort(){ return sourceAppPort; }
+	public int getDestPort(){ return destAppPort; }
+}
